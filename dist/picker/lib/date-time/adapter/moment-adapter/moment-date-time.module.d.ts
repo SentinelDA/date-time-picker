adbf1537107e00ed11a3efbc3a4ecc45dbbdbb3c
Nathan Eichenlaub <nathane@sentinelda.com>
2020-03-18 19:35:02 -0600

    add /dist back to .gitignore

--- dist/picker/lib/date-time/adapter/moment-adapter/moment-date-time.module.d.ts
+++
@@ -1,9 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class MomentDateTimeModule {
-    static ɵmod: i0.ɵɵNgModuleDefWithMeta<MomentDateTimeModule, never, never, never>;
-    static ɵinj: i0.ɵɵInjectorDef<MomentDateTimeModule>;
-}
-export declare class OwlMomentDateTimeModule {
-    static ɵmod: i0.ɵɵNgModuleDefWithMeta<OwlMomentDateTimeModule, never, [typeof MomentDateTimeModule], never>;
-    static ɵinj: i0.ɵɵInjectorDef<OwlMomentDateTimeModule>;
-}
